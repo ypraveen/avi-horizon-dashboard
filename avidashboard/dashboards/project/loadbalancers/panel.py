9c2cff509832c83eed4e220350fbcf73d28f9bec
Remo Mattei <remo@mattei.org>
2019-06-05 09:07:42 -0700

    Moving 1490 to 1491 OSP has already a 1490, conflict fixed Chaning values because openstack_local cannot be changed in the framework on new values not ported by Horizon

--- avidashboard/dashboards/project/loadbalancers/panel.py
+++ avidashboard/dashboards/project/loadbalancers/panel.py
@@ -5,15 +5,15 @@ import horizon
 
 
 class AviLBaaSPanel(horizon.Panel):
-    name = _(getattr(settings, 'AVI_LBAAS_PANEL_NAME', "Load Balancers"))
+    name = _(getattr(settings, 'AVI_LBAAS_PANEL_NAME', "Avi Load Balancers"))
     slug = 'aviloadbalancers'
     permissions = ('openstack.services.network',)
 
     @staticmethod
     def can_register():
         # network_config = getattr(settings, 'OPENSTACK_NEUTRON_NETWORK', {})
-        full_ui = getattr(settings, 'AVI_LBAAS_FULL_UI', False)
-        readonly_ui = getattr(settings, 'AVI_LBAAS_FULL_READONLY_UI', False)
+        full_ui = getattr(settings, 'AVI_LBAAS_FULL_UI', True)
+        readonly_ui = getattr(settings, 'AVI_LBAAS_FULL_READONLY_UI', True)
         # return network_config.get('enable_lb', True) and full_ui
         return full_ui or readonly_ui
 
